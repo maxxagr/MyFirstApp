0b516b7e0a9c64c6cd7e52483d9f3ee3ee712766
unknown <maxxagr@gmail.com>
2021-11-24 00:52:37 +0500

    initial commit2

--- app/src/main/java/com/maxxon/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/maxxon/myfirstapp/MainActivity.kt
@@ -7,11 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        dsvdsgvdsvdsvdvdsvdsvd
-        piouoiuoiu
-        iouuoi
-        kujhukhuoihuio
-        iohoihoi
+
 
 
 
